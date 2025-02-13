fd0dbddbe629269106bbeccb297b8d25821ecda4
Piotr <pbabik2024@gmail.com>
2025-02-13 19:45:31 +0100

    finished menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+// HEADER
 const header = document.querySelector('.header')
 
+// NAV
 const nav = document.querySelector('.nav')
 const burger = document.querySelector('.nav__menu-mobile')
-
 const burgerIcon = document.querySelector('.nav__burger-icon')
 const burgerItems = document.querySelectorAll('.nav__menu-mobile-item')
 
+// MENU
 const menuCategoryItems = document.querySelectorAll('.menu__category-item')
-const menuSearchBar = document.querySelector('.menu__search-bar-input')
-const manuItemsTitle = document.querySelectorAll('.menu__item-title')
+const menuNeapol = document.querySelector('.menu__box-neapol')
+const menuRome = document.querySelector('.menu__box-rome')
+const menuSicily = document.querySelector('.menu__box-sicily')
 
 const blockScroll = () => {
 	burger.classList.toggle('showNav')
@@ -31,22 +34,23 @@ const showNavBar = () => {
 const menuCategory = e => {
 	menuCategoryItems.forEach(el => el.classList.remove('menu__category-item--active'))
 	e.target.classList.toggle('menu__category-item--active')
-}
 
-const menuSearch = () => {
-	manuItemsTitle.forEach(el => {
-		const parentBox = el.closest('.menu__item')
-
-		if (!el.textContent.toLocaleLowerCase().includes(menuSearchBar.value.toLowerCase())) {
-			parentBox.style.display = 'none'
-		} else {
-			parentBox.style.display = 'block'
-		}
-	})
+	if (e.target.classList.contains('neapol-button')) {
+		menuNeapol.classList.add('menu__box--active')
+		menuRome.classList.remove('menu__box--active')
+		menuSicily.classList.remove('menu__box--active')
+	} else if (e.target.classList.contains('rome-button')) {
+		menuRome.classList.add('menu__box--active')
+		menuNeapol.classList.remove('menu__box--active')
+		menuSicily.classList.remove('menu__box--active')
+	} else {
+		menuSicily.classList.add('menu__box--active')
+		menuNeapol.classList.remove('menu__box--active')
+		menuRome.classList.remove('menu__box--active')
+	}
 }
 
 menuCategoryItems.forEach(el => el.addEventListener('click', menuCategory))
 window.addEventListener('scroll', showNavBar)
 burgerIcon.addEventListener('click', blockScroll)
 burgerItems.forEach(el => el.addEventListener('click', blockScroll))
-menuSearchBar.addEventListener('input', menuSearch)
